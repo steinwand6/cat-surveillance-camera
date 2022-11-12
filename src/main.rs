97d5a06dab62251441c184a6215073ca5b5ab0e8
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-11-12 23:21:07 +0900

    add rppal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rppal::gpio::{Gpio, InputPin, Level, OutputPin};
+use std::error::Error;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+const GPIO17: u8 = 17;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut pir = Gpio::new()?.get(GPIO17)?.into_input();
+    pir.set_interrupt(rppal::gpio::Trigger::Both)?;
+
+    loop {
+        match pir.poll_interrupt(true, None) {
+            Ok(trigger) => match trigger {
+                Some(rppal::gpio::Level::High) => {
+                    println!("!!");
+                }
+                Some(rppal::gpio::Level::Low) => {
+                    println!("...");
+                }
+                None => (),
+            },
+            _ => break,
+        }
+    }
+    Ok(())
 }
