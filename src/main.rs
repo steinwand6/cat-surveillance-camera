3b0481f66bb8be412356843460f580edf66d86df
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-11-13 16:48:18 +0900

    fix interrupt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use chrono::Local;
 use reqwest::blocking::{multipart, Client};
-use rppal::gpio::{Gpio, Level};
+use rppal::gpio::Gpio;
 use std::{env, error::Error, process::Command};
 
 const GPIO17: u8 = 17;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pir = Gpio::new()?.get(GPIO17)?.into_input();
-    pir.set_interrupt(rppal::gpio::Trigger::Both)?;
+    pir.set_interrupt(rppal::gpio::Trigger::RisingEdge)?;
 
     let line_token =
         env::var("LINE_TOKEN").expect("LINE_TOKEN is empty. Set the access token to LINE_TOKEN");
@@ -15,28 +15,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     loop {
         match pir.poll_interrupt(true, None) {
-            Ok(trigger) => match trigger {
-                Some(Level::High) => {
-                    let dt = Local::now();
-                    let file_name =
-                        format!("{}/image_{}.jpg", image_dir, dt.format("%Y%m%d%H%M%S"));
-                    Command::new("libcamera-jpeg")
-                        .args(["-o", file_name.as_str()])
-                        .output()?;
-                    println!("!!");
-                    let client = Client::new();
-                    let form = multipart::Form::new()
-                        .text("message", "Detected")
-                        .file("imageFile", file_name)?;
-                    let req = client
-                        .post("https://notify-api.line.me/api/notify")
-                        .bearer_auth(&line_token)
-                        .multipart(form);
-                    let res = req.send()?;
-                    println!("{:?}", res);
-                }
-                _ => (),
-            },
+            Ok(_) => {
+                let dt = Local::now();
+                let file_name = format!("{}/image_{}.jpg", image_dir, dt.format("%Y%m%d%H%M%S"));
+                Command::new("libcamera-jpeg")
+                    .args(["-o", file_name.as_str()])
+                    .output()?;
+                println!("!!");
+                let client = Client::new();
+                let form = multipart::Form::new()
+                    .text("message", "Detected")
+                    .file("imageFile", file_name)?;
+                let req = client
+                    .post("https://notify-api.line.me/api/notify")
+                    .bearer_auth(&line_token)
+                    .multipart(form);
+                let res = req.send()?;
+                println!("{:?}", res);
+            }
             _ => break,
         }
     }
