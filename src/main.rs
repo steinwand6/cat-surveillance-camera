4364149b317b7aa51cb98f30ba0fde4c9e1940b4
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-11-13 13:58:06 +0900

    add reqwest and env

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use chrono::Local;
+use reqwest::blocking::Client;
 use rppal::gpio::{Gpio, Level};
-use std::{error::Error, process::Command};
+use std::{env, error::Error, process::Command};
 
 const GPIO17: u8 = 17;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pir = Gpio::new()?.get(GPIO17)?.into_input();
     pir.set_interrupt(rppal::gpio::Trigger::Both)?;
+
+    let line_token =
+        env::var("LINE_TOKEN").expect("LINE_TOKEN is empty. Set the access token to LINE_TOKEN");
 
     let image_dir = "/tmp/cat-sv";
 
@@ -21,6 +25,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .args(["-o", file_name.as_str()])
                         .output()?;
                     println!("!!");
+                    let client = Client::new();
+                    let _ = client
+                        .post("https://notify-api.line.me/api/notify")
+                        .header(
+                            reqwest::header::AUTHORIZATION,
+                            format!("bearer {}", line_token),
+                        )
+                        .body(format!("message=Detected! imageFiles=@{}", file_name))
+                        .send();
                 }
                 _ => (),
             },
